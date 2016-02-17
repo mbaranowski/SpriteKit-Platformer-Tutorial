4567539f533b91b8c9197723e69c331affa9af50
Matthew Baranowski <matt.baranowski@willowtreeapps.com>
2016-02-17 14:49:56 -0500

    Player Update

--- PlatformerDemo/Player.swift
+++ PlatformerDemo/Player.swift
@@ -10,6 +10,20 @@ import Foundation
 
 class Player : SKSpriteNode {
     
+    var desiredPosition : CGPoint = CGPoint.zero
+    var velocity : CGPoint = CGPoint.zero
+    
+    var onGround : Bool = false
+    var forward : Bool = false
+    var backward : Bool = false
+    var shouldJump : Bool = false
+    
+    static let gravity = CGPoint(x: 0.0, y: -450.0)
+    static let forwardMove = CGPoint(x:1000.0, y:0.0)
+    static let dampeningForce = CGPoint(x: 0.9, y: 1.0)
+    static let jumpForce = CGPoint(x:0.0, y:400)
+    static let jumpCutoff : CGFloat = 150.0
+    
     init() {
         let texture = SKTexture(imageNamed: "p1_stand")
         super.init(texture: texture, color: .whiteColor(), size:texture.size() )
@@ -20,5 +34,28 @@ class Player : SKSpriteNode {
     }
     
     func update(delta : NSTimeInterval) {
+        let gravityStep = Player.gravity * delta
+        let forwardStep = Player.forwardMove * delta
+        
+        self.velocity = (self.velocity + gravityStep) * Player.dampeningForce;
+        
+        if self.shouldJump && self.onGround {
+            self.velocity = self.velocity + Player.jumpForce
+        } else if !self.shouldJump && self.velocity.y > Player.jumpCutoff {
+            self.velocity = CGPoint(x:self.velocity.x, y:Player.jumpCutoff)
+        }
+        
+        if self.forward {
+            self.velocity = self.velocity + forwardStep
+        }
+        
+        if self.backward {
+            self.velocity = self.velocity - forwardStep
+        }
+        
+        let facingBack = self.velocity.x < 0
+        self.xScale = facingBack ? -1.0 : 1.0
+        
+        self.desiredPosition = self.position + (self.velocity * delta)
     }
 }
